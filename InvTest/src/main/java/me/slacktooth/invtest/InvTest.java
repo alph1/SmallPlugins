d93da217bab922fd6ee81edca99ea2c809a0d0b5
slacktooth <hawkrobin@outlook.com>
2016-12-03 15:38:01 -0500

    Add InvTest

---
+++ InvTest/src/main/java/me/slacktooth/invtest/InvTest.java
@@ -0,0 +1,11 @@
+package me.slacktooth.invtest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InvTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
---
+++ InvTest/src/main/java/me/slacktooth/invtest/InvTestListener.java
@@ -0,0 +1,30 @@
+package me.slacktooth.invtest;
+
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
+public class InvTestListener implements Listener {
+
+    private HashMap<Player, ItemStack[]> inventory = new HashMap<Player, ItemStack[]>();
+
+    @EventHandler
+    public void onPlayerGamemodeChange(PlayerGameModeChangeEvent event) {
+        Player player = event.getPlayer();
+        if (player.getGameMode() == GameMode.CREATIVE) {
+            inventory.put(player, player.getInventory().getContents());
+            player.getInventory().clear();
+        }
+        if (player.getGameMode() == GameMode.SURVIVAL) {
+            if(inventory.containsKey(player)) {
+                player.getInventory().setContents(inventory.get(player));
+            }
+        }
+    }
+}
